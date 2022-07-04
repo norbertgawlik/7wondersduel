587a1c3d51b21f49cd479d7cfe8fdbd431ece5b8
norbertgawlik <norbert.gawlik95@gmail.com>
2022-07-04 22:23:44 +0200

    switch repo files

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,72 @@
-console.log("test");
+import { Player } from "./Player.js";
+class Game{
+    constructor(){
+        this.cards = {};
+        this.player1 = new Player("Gracz1");
+        this.player2 = new Player("Gracz2");
+        this.configCards = {
+            'age1' : [2,3,4,5,6]
+        }
+    }
+
+    init(){
+        this.loadCards('js/cards.json');
+    }
+
+    loadCards(cards){
+        fetch(cards)
+            .then((parse)=>parse.json())
+            .then((json)=>{
+                this.cards = json;
+                this.loadAge("age1");
+            })
+            .catch((e)=>console.log(e));
+        }
+    loadAge(age){
+        const wrap = document.querySelector('#cards-board');
+        if(age = "age1"){
+            let counterCards = 20;
+            const cards = this.drawCards(this.cards.age1,20);
+            const config = this.configCards[age];
+            console.log(config.length);
+            for(let i=0;i<config.length;i++){
+                const row = document.createElement('div');
+                row.classList.add('row');
+                row.classList.add('center');
+                wrap.appendChild(row);
+                for(let j=0;j<config[i];j++){
+                    const card = document.createElement('div');
+                    card.classList.add('card');
+
+                    row.appendChild(card);
+
+                    if(config[i][j] != 0){
+                        console.log(cards[counterCards-1]);
+                        card.setAttribute('data-cardboard-id',counterCards-1);
+                        card.setAttribute('data-color',cards[counterCards-1].color);
+                        card.setAttribute('data-type',cards[counterCards-1].type);
+                        const name = document.createElement('span');
+                        name.classList.add('name');
+                        name.innerHTML = cards[counterCards-1].name;
+                        card.appendChild(name);
+                        counterCards--;
+                    }
+                }
+
+            }
+        }
+    }
+    drawCards(set,max){
+        const shuffled = set.sort(() => 0.5 - Math.random());
+        return shuffled.slice(0,max);
+    }
+}
+
+const newGame = new Game();
+document.addEventListener('DOMContentLoaded',()=>{
+    newGame.init();
+})
+
+
+/* */
+/* konfi kart talii */
---
+++ js/Player.js
@@ -0,0 +1,27 @@
+export class Player{
+    constructor(name){
+        this.name = name;
+        this.money = 7;
+        this.points = 0;
+        this.strategy = 0;
+
+        this.tokens = {
+            "count" : 0,
+            "agriculture" : false, /* 6$ + 4pkt */
+            "architecture" : false, /* 2 surowce zignorowane przy bydowie cudów */
+            "economy" : false, /* $ wydane na surowce trafiają do enemy */
+            "law" : false, /* scientific symbol */
+            "masonry" : false, /* 2 surowce zignorowane przy zakupie blue card*/
+            "mathematics" : false, /* 3pkt za kazdy token */
+            "philosophy" : false, /* 7pkt */
+            "strategy" : false, /*kazda karta wojny +1 extra shield */
+            "theology" : false, /* kazdy cud "play again" */
+            "urbanism" : false, /* 6$ + 4$ za kazdy free build */
+        }
+    }
+
+    checkMoney(){
+        console.log(`${this.name} - ${this.money}`);
+    }
+
+}
